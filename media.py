b0dc96a6aee87fa35258e7dc9f4c49ae66309cf9
Darren Abramson <da@dal.ca>
2015-08-18 20:04:28 -0300

    My code is now PEP8 compliant.

--- media.py
+++ media.py
@@ -10,8 +10,11 @@ import getmetascore
 # getmetascore.getscore is called to retrieve the metascore.
 # Notice that the normal, non-web-formatted version of the
 # movie title is passed.
+
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_you
+                 tube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -20,7 +23,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-   
-        
-    
