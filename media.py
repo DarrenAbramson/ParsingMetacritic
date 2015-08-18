5c42dc17c6bc82dc50eff49741be5e218dbd4d18
Darren Abramson <da@dal.ca>
2015-08-18 20:13:52 -0300

    Fixed errors introduced by PEP8 compliance..

--- media.py
+++ media.py
@@ -13,8 +13,7 @@ import getmetascore
 
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_you
-                 tube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
